5f9c37da2cc23276fc9861cea95928fc628f1ffb
Xian-Wang-Zion <xwang2424@wisc.edu>
2021-11-03 10:32:12 -0500

    Moody's firebase backend

---
+++ app/src/main/java/com/example/moody/FindNewFriendsFragment.java
@@ -0,0 +1,19 @@
+package com.example.moody;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FindNewFriendsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.find_new_friends_fragment, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/moody/LoginFragment.java
@@ -0,0 +1,56 @@
+package com.example.moody;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LoginFragment extends Fragment {
+    Button login_button;
+    EditText login_username, login_password;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.login_fragment, container, false);
+        login_button = view.findViewById(R.id.login_button);
+        login_username = view.findViewById(R.id.login_username);
+        login_password = view.findViewById(R.id.login_password);
+
+        TextView registerBtn  = view.findViewById(R.id.goToRegister);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RegisterFragment registerFragment = new RegisterFragment();
+                FragmentManager manager = getActivity().getSupportFragmentManager();
+                manager.beginTransaction().replace(R.id.fragment_container, registerFragment, registerFragment.getTag()).commit();
+            }
+        });
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+                reference.setValue("change made to backend");
+            }
+        });
+
+
+
+        return view;
+    }
+}
