7c4ca1f3ef7e90d2982475654b87ec26134aa9d3
Xian-Wang-Zion <xwang2424@wisc.edu>
2021-11-05 19:32:41 -0500

    Profile and Profile Edit and Login bug with change password

--- app/src/main/java/com/example/moody/ProfileEditFragment.java
+++ app/src/main/java/com/example/moody/ProfileEditFragment.java
@@ -10,22 +10,48 @@ import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Calendar;
+import java.util.HashMap;
 
 public class ProfileEditFragment extends Fragment{
     DatePickerDialog.OnDateSetListener listener;
-    TextView birthday_date;
+    TextView birthday_date, done;
+
+
+    EditText firstName, lastName, username, password;
+
+    private FirebaseUser user;
+    private DatabaseReference ref;
+    private String Uid;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.home_fragment, container, false);
+        View view = inflater.inflate(R.layout.profile_edit_fragment, container, false);
         birthday_date = (TextView) view.findViewById(R.id.birthday_date);
+
+        done = view.findViewById(R.id.editDone);
+        firstName = view.findViewById(R.id.editFirstName);
+        lastName = view.findViewById(R.id.editLastName);
+        username = view.findViewById(R.id.editUsername);
+        password = view.findViewById(R.id.editPassword);
+
+
+
 
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
@@ -54,7 +80,51 @@ public class ProfileEditFragment extends Fragment{
             }
         };
 
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fn = firstName.getText().toString().trim();
+                String ln = lastName.getText().toString().trim();
+                String un = username.getText().toString().trim();
+                String pw = password.getText().toString().trim();
+
+                updateProfile(fn, ln, un, pw);
+            }
+        });
+
+
         return view;
+    }
+
+    public void updateProfile(String fn, String ln, String un, String pw){
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        ref = FirebaseDatabase.getInstance().getReference("Users");
+        Uid = user.getUid();
+
+        HashMap User = new HashMap();
+        User.put("firstName", fn);
+        User.put("lastName", ln);
+        User.put("username", un);
+        User.put("password", pw);
+
+        ref.child(Uid).updateChildren(User).addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if(task.isSuccessful()){
+                    ProfileFragment profileFragment = new ProfileFragment();
+                    FragmentManager manager = getActivity().getSupportFragmentManager();
+                    manager.beginTransaction().replace(R.id.fragment_container, profileFragment, profileFragment.getTag()).commit();
+                    Toast.makeText(getActivity(), "Update successfully", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getActivity(), "Update failed", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+
+
+
     }
 
 
