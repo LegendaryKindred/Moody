ba353b5376dde247efc24ecce02e0997cbf41769
ryang96 <47955402+ryang96@users.noreply.github.com>
2021-11-16 23:36:42 -0600

    change google map

--- app/src/main/java/com/example/moody/HomeFragment.java
+++ app/src/main/java/com/example/moody/HomeFragment.java
@@ -6,6 +6,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -13,6 +15,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class HomeFragment extends Fragment {
+    GoogleMap map;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,6 +29,12 @@ public class HomeFragment extends Fragment {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
                 //when map is loaded
+                LatLng madison = new LatLng(43.0730517,-89.4012302);
+                map = googleMap;
+                map.addMarker(new MarkerOptions().position(madison).title("madison"));
+                map.moveCamera(CameraUpdateFactory.newLatLng(madison));
+                map.setMinZoomPreference(12);
+
                 googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener(){
                     @Override
                     public void onMapClick(@NonNull LatLng latLng) {
