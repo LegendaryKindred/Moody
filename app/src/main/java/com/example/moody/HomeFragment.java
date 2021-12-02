5cd8591c357ca5fcdaa9f149cb5e1bae52a481e0
ryang96 <47955402+ryang96@users.noreply.github.com>
2021-12-01 21:09:19 -0600

    “1”

--- app/src/main/java/com/example/moody/HomeFragment.java
+++ app/src/main/java/com/example/moody/HomeFragment.java
@@ -29,6 +29,8 @@ public class HomeFragment extends Fragment {
     SupportMapFragment supportMapFragment;
 
     @Nullable
+
+    
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragment, container, false);
