6b7f1f794bcc7e34644081914f86e61f534d09e9
ryang96 <47955402+ryang96@users.noreply.github.com>
2021-12-03 14:23:32 -0600

    update status

--- app/src/main/java/com/example/moody/status.java
+++ app/src/main/java/com/example/moody/status.java
@@ -1,13 +1,27 @@
 package com.example.moody;
+import android.location.Location;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class status {
     private String date;
     private String message;
     private int rating;
+    private LatLng latLng;
 
-    public status(String date,String massage,int rating){
+    public status(String date,String massage,int rating,LatLng latLng){
         this.date=date;
         this.message=message;
         this.rating=rating;
+        this.latLng=latLng;
     }
 }
