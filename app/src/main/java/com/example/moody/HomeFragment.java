ee8e7613821da65cae38e40a0dfbed0e53aa5391
JiweonYoo <1029jiweon@gmail.com>
2021-12-10 16:41:50 -0600

    pop up dialog, add button on map

--- app/src/main/java/com/example/moody/HomeFragment.java
+++ app/src/main/java/com/example/moody/HomeFragment.java
@@ -1,9 +1,14 @@
 package com.example.moody;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +25,9 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragment, container, false);
+
+        ImageButton btnShowDialog = view.findViewById(R.id.showDialog);
+
 
         //Google Map
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.googleMap);
@@ -44,7 +52,32 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        btnShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
 
         return view;
+    }
+
+    private void showDialog() {
+//        Toast.makeText(getView().getContext(), "clicked", Toast.LENGTH_SHORT).show();
+        Dialog dialog = new Dialog(getContext());
+        ImageView btncloseDialog = dialog.findViewById(R.id.closeDialog);
+        dialog.setContentView(R.layout.pop_up_rating);
+        dialog.show();
+
+        // close the dialog
+//        btncloseDialog.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                dialog.dismiss();
+//            }
+//        });
+
+
+
     }
 }
